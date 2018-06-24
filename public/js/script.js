3ce4d9523086ac1c910a88c2415401573e7a83e4
YujiYabe <yayuji@gmail.com>
2018-06-24 11:43:10 +0900

    add wait gif

--- public/js/script.js
+++ public/js/script.js
@@ -9,7 +9,7 @@ new Vue({
 		targetTranslatePhrase: '',
 
 		isShowPreview: false,
-
+		// wait: true,
 		isShowButtonScanFile: false,
 		isShowZoneScanedText: false,
 		isShowZoneDoneTranslate: false,
@@ -26,36 +26,14 @@ new Vue({
 		} else {
 			this.targetLanguageSymbole = "es";
 		}
+		// this.wait = false;
+		this.displayWaitImage(false);
 
 	},
 	methods: {
 
-		speechText: function (event) {
-			speechSynthesis.cancel();
 
-			var langPair = {};
-			langPair.ja = 'ja-JP';
-			langPair.en = 'en-US';
-			langPair.es = 'es-ES';
-			langPair.fr = 'fr-FR';
-			langPair.zh = 'zh-CN';
-			langPair.ru = 'ru-RU';
-			langPair.ko = 'ko-KO';
-			langPair.ar = 'ar-AR';
-			var targetLanguageSymbole = this.targetLanguageSymbole;
-
-			var synthes = new SpeechSynthesisUtterance();
-			synthes.voiceURI = 'native';
-			synthes.volume = 1;
-			synthes.rate = 0.9;
-			synthes.pitch = 0;
-			synthes.text = this.targetTranslatePhrase;
-			synthes.lang = langPair[targetLanguageSymbole];
-
-			speechSynthesis.speak(synthes);
-
-		},
-
+		//=========================================================================-
 		canvasDraw: function (event) {
 			var file = document.getElementById('imageSelect').files[0];
 
@@ -106,118 +84,10 @@ new Vue({
 
 		},
 
-		// 引数のBase64の文字列をBlob形式にする
-		base64ToBlob: function (base64, event) {
-			var base64Data = base64.split(',')[1], // Data URLからBase64のデータ部分のみを取得
-				data = window.atob(base64Data), // base64形式の文字列をデコード
-				buff = new ArrayBuffer(data.length),
-				arr = new Uint8Array(buff),
-				blob,
-				i,
-				dataLen;
-			// blobの生成
-			for (i = 0, dataLen = data.length; i < dataLen; i++) {
-				arr[i] = data.charCodeAt(i);
-			}
-			blob = new Blob([arr], { type: 'image/jpeg' });
-			return blob;
-
-		},
-
-		unescapeHTML: function (str, event) {
-			var returnstr = str
-				.replace(/&/g, "&amp;")
-				.replace(/'/g, "&#x27;")
-				.replace(/`/g, "&#x60;")
-				.replace(/"/g, "&quot;")
-				.replace(/</g, "&lt;")
-				.replace(/>/g, "&gt;")
-				.replace(/\r/g, "&#13;")
-				.replace(/\n/g, "&#10;")
-
-				; //-----------------------
-			return returnstr;
-		},
-
-		escapeHTML: function (str, event) {
-			var returnstr = str
-				.replace(/&amp;/g, "&")
-				.replace(/&#x27;/g, "'")
-				.replace(/&#39;/g, "'")
-				.replace(/&#x60;/g, "`")
-				.replace(/&quot;/g, '"')
-				.replace(/&lt;/g, "<")
-				.replace(/&gt;/g, ">")
-				.replace(/&#13;/g, "\r")
-				.replace(/&#10;/g, "\n")
-				; //-----------------------
-			return returnstr;
-		},
-
-		pushSourceTranslatePhrase: function ( event) {
-			localStorage.sourceTranslatePhrase = this.sourceTranslatePhrase;
-		},
-
-
-		pushSelectedLanguage: function (langName, event) {
-			var selectLang = langName;
-			this.targetLanguageSymbole = selectLang;
-			localStorage.selectedLanguage = selectLang;
-		},
-
-		translateText: function (event) {
-			var langPair = {};
-			langPair.ja = 'jp';
-			langPair.en = 'en';
-			langPair.es = 'es';
-			langPair.fr = 'fr';
-			langPair.zh = 'zh';
-			langPair.ru = 'ru';
-
-			localStorage.sourceTranslatePhrase = this.sourceTranslatePhrase;
-
-			var sourceTranslatePhrase = document.getElementById('sourceTranslatePhrase').innerText;
-			var targetLanguageSymbole = this.targetLanguageSymbole;
-			var sourceLanguageSymbole = this.sourceLanguageSymbole;
-
-
-			if (langPair[sourceLanguageSymbole] == targetLanguageSymbole) {
-
-				this.isShowZoneDoneTranslate = true;
-
-				var unescapestr = this.unescapeHTML(targetTranslate);
-
-				this.targetTranslatePhrase = unescapestr;
-
-				return;
-			} else {
-
-				var post_data = new URLSearchParams();
-				post_data.append('sourceTranslatePhrase', sourceTranslatePhrase);
-				post_data.append('targetLanguageSymbole', targetLanguageSymbole);
-
-				axios.post('translate', post_data)
-					.then(function (res) {
-						this.isShowZoneDoneTranslate = true;
-
-						var unescapestr = this.escapeHTML(res.data);
-						console.log(unescapestr);
-
-						localStorage.targetTranslatePhrase = unescapestr;
-
-						this.targetTranslatePhrase = localStorage.targetTranslatePhrase;
-
-
-					}.bind(this)).catch(function (err) {
-						console.log(err);
-					}.bind(this));
-
-
-			}
-		},
-
+		//=========================================================================-
 		imageUpload: function (event) {
-
+			// this.wait = true;
+			this.displayWaitImage(true);
 			var form = document.getElementById('imageForm');
 			var formData = new FormData(form);
 
@@ -247,6 +117,7 @@ new Vue({
 				//アップロード用blobをformDataに追加
 				formData.append("selectImage", uploadBlob);
 			}
+			// console.log(this.wait);
 
 			axios.post('scan', formData)
 				.then(function (res) {
@@ -258,12 +129,190 @@ new Vue({
 					// document.getElementById('targetTranslatePhrase').innerText = sourceTranslatePhrase ; 
 
 					this.isShowZoneScanedText = true;
+					// this.wait = false;
+					this.displayWaitImage(false);
 
 				}.bind(this)).catch(function (err) {
 					console.log(err);
+					// this.wait = false;
+					this.displayWaitImage(false);
 				}.bind(this));
 
 		},
+
+
+
+		//=========================================================================-
+		translateText: function (event) {
+			this.displayWaitImage(true);
+			var langPair = {};
+			langPair.ja = 'jp';
+			langPair.en = 'en';
+			langPair.es = 'es';
+			langPair.fr = 'fr';
+			langPair.zh = 'zh';
+			langPair.ru = 'ru';
+
+			localStorage.sourceTranslatePhrase = this.sourceTranslatePhrase;
+
+			var sourceTranslatePhrase = document.getElementById('sourceTranslatePhrase').innerText;
+			var targetLanguageSymbole = this.targetLanguageSymbole;
+			var sourceLanguageSymbole = this.sourceLanguageSymbole;
+
+
+			if (langPair[sourceLanguageSymbole] == targetLanguageSymbole) {
+
+				this.isShowZoneDoneTranslate = true;
+
+				var unescapestr = this.unescapeHTML(targetTranslate);
+
+				this.targetTranslatePhrase = unescapestr;
+				this.displayWaitImage(false);
+
+				return;
+			} else {
+				// this.wait = true;
+	
+				var post_data = new URLSearchParams();
+				post_data.append('sourceTranslatePhrase', sourceTranslatePhrase);
+				post_data.append('targetLanguageSymbole', targetLanguageSymbole);
+				// console.log(this.wait);
+				axios.post('translate', post_data)
+					.then(function (res) {
+						this.isShowZoneDoneTranslate = true;
+
+						var unescapestr = this.escapeHTML(res.data);
+						console.log(unescapestr);
+
+						localStorage.targetTranslatePhrase = unescapestr;
+
+						this.targetTranslatePhrase = localStorage.targetTranslatePhrase;
+						this.displayWaitImage(false);
+
+						// this.wait = false;
+					}.bind(this)).catch(function (err) {
+						this.displayWaitImage(false);
+						console.log(err);
+					}.bind(this));
+				// this.wait = false;
+
+			}
+		},
+
+		//=========================================================================-
+		speechText: function (event) {
+			this.displayWaitImage(true);
+			speechSynthesis.cancel();
+
+			var langPair = {};
+			langPair.ja = 'ja-JP';
+			langPair.en = 'en-US';
+			langPair.es = 'es-ES';
+			langPair.fr = 'fr-FR';
+			langPair.zh = 'zh-CN';
+			langPair.ru = 'ru-RU';
+			langPair.ko = 'ko-KO';
+			langPair.ar = 'ar-AR';
+			var targetLanguageSymbole = this.targetLanguageSymbole;
+
+			var synthes = new SpeechSynthesisUtterance();
+			synthes.voiceURI = 'native';
+			synthes.volume = 1;
+			synthes.rate = 0.9;
+			synthes.pitch = 0;
+			synthes.text = this.targetTranslatePhrase;
+			synthes.lang = langPair[targetLanguageSymbole];
+
+			speechSynthesis.speak(synthes);
+			this.displayWaitImage(false);
+
+		},
+
+		//=========================================================================-
+		pushSourceTranslatePhrase: function (event) {
+			localStorage.sourceTranslatePhrase = this.sourceTranslatePhrase;
+		},
+
+
+		//=========================================================================-
+		pushSelectedLanguage: function (langName, event) {
+			var selectLang = langName;
+			this.targetLanguageSymbole = selectLang;
+			localStorage.selectedLanguage = selectLang;
+		},
+
+		//=========================================================================-
+		unescapeHTML: function (str, event) {
+			var returnstr = str
+				.replace(/&/g, "&amp;")
+				.replace(/'/g, "&#x27;")
+				.replace(/`/g, "&#x60;")
+				.replace(/"/g, "&quot;")
+				.replace(/</g, "&lt;")
+				.replace(/>/g, "&gt;")
+				.replace(/\r/g, "&#13;")
+				.replace(/\n/g, "&#10;")
+
+				; //-----------------------
+			return returnstr;
+		},
+
+		//=========================================================================-
+		escapeHTML: function (str, event) {
+			var returnstr = str
+				.replace(/&amp;/g, "&")
+				.replace(/&#x27;/g, "'")
+				.replace(/&#39;/g, "'")
+				.replace(/&#x60;/g, "`")
+				.replace(/&quot;/g, '"')
+				.replace(/&lt;/g, "<")
+				.replace(/&gt;/g, ">")
+				.replace(/&#13;/g, "\r")
+				.replace(/&#10;/g, "\n")
+				; //-----------------------
+			return returnstr;
+		},
+
+		// 引数のBase64の文字列をBlob形式にする
+		//=========================================================================-
+		base64ToBlob: function (base64, event) {
+			var base64Data = base64.split(',')[1], // Data URLからBase64のデータ部分のみを取得
+				data = window.atob(base64Data), // base64形式の文字列をデコード
+				buff = new ArrayBuffer(data.length),
+				arr = new Uint8Array(buff),
+				blob,
+				i,
+				dataLen;
+			// blobの生成
+			for (i = 0, dataLen = data.length; i < dataLen; i++) {
+				arr[i] = data.charCodeAt(i);
+			}
+			blob = new Blob([arr], { type: 'image/jpeg' });
+			return blob;
+
+		},
+		//=========================================================================-
+		displayWaitImage: function (isWait ,event) {
+			if (isWait) {
+				document.getElementById('waitImageZone').style.display='block';
+				document.getElementById('applicationZone').style.display='none';
+			} else {
+				document.getElementById('waitImageZone').style.display='none';
+				document.getElementById('applicationZone').style.display='block';
+			}
+		},
+
+
+		// //=========================================================================-
+		// testClick: function (event) {
+		// 	if (this.wait) {
+		// 		this.wait = false;
+		// 	} else {
+		// 		this.wait = true;
+		// 	}
+		// },
+
+
 	}
 })
 
